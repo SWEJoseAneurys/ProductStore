21b0f96446d373637c95e53cf5f68271182ca5d9
Jose Castro <JoseAneurys@me.com>
2023-02-24 00:38:08 -0500

    Code cleaning -- edit/editSnack complete

--- public/edit/editSnack/index.js
+++ public/edit/editSnack/index.js
@@ -1,15 +1,12 @@
 //Navbar; create buttons for navigation to respective pages
 let homepageClick = document.getElementById('homepage-click');
 let createNavClick = document.getElementById('create-click');
-const updatingSnack = document.getElementById('updating-snack');
 
 //get the id of snack from the URL
 const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
 });
 let id = params.id;
-console.log(id);
-
 
 //create action when clicking to go back to homepage
 homepageClick.addEventListener('click', () => {
@@ -23,33 +20,19 @@ createNavClick.addEventListener('click', async () => {
     window.location.href = "../../create"
 });
 
-// const updateSnack = async () => {
-//     //grab snack data from edit
-//     let response = await fetch(`http://localhost:5000/edit/editSnack/`, {
-//         method: 'PUT',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         // get id and other data from your front end usind document.getElementById for example
-//         // body: JSON.stringify(dataObject)
-//     }
-//     );
+//create the update button to submit changes to snack
+let updateButton = document.getElementById('update-button');
 
-//     let finalData = await response.json();
-
-//     console.log(finalData);
-//     // use this finalData to m
-// }
-
-let updateButton = document.getElementById('update-button')
 //create action when clicking to update snack
 updateButton.addEventListener('click', async () => {
+    //set input values on front-end(HTML) to respective variables
     let snack = document.getElementById('type-input').value;
     let price = +document.getElementById('price-input').value;
     let image = document.getElementById('img-input').value;
     let quantity = document.getElementById('quantity-input').value;
+    // create snack object containing properties above that will define/describe
     const snackObject = {
-    } 
+    }//write if statements that prove true to take in new value from front-end as defined  
     if (snack){
         snackObject.snack = snack;
     }
@@ -62,34 +45,29 @@ updateButton.addEventListener('click', async () => {
     if (quantity){
         snackObject.quantity = quantity;
     }
-    console.log("updating");
+    //make call to server to update existing snack with newly entered data
     let response = await fetch(`http://localhost:5000/edit/editSnack/${id}`, {
+        //because we are editing/PUTting new value into already established fields of snack, method for updating will be "PUT"
         method: "PUT",
+        //define what is passed so that it is readable
         headers: {
             'Content-Type': 'application/json',
         },
         // to send JSON data over HTTP
         body: JSON.stringify(snackObject)
-    })
-    console.log(response)
-
-
-    let submissionStatusTag = document.getElementById('submission-status');
-    console.log(response.status);
+    });
+    //communicate that the update was successful
+    let updateStatusTag = document.getElementById('update-status');
     if (response.status === 200) {
-        console.log(response);
-        console.log("Great! Your snack has been updated!!!");
-        submissionStatusTag.textContent = "Updated!";
-        submissionStatusTag.style.color = "green";
+        console.log("Updated!");
+        updateStatusTag.textContent = "Great! Your snack has been updated!!!";
+        updateStatusTag.style.color = "green";
     } else {
         console.log(response);
-        console.log("Uh-oh! Something went wrong..");
-        window.prompt("Oops! Something went wrong..try again")
-        console.log;
-        uploadStatusTag.textContent = "Try again";
-        uploadStatusTag.style.color = "red";
+        updateStatusTag.textContent = "Try again";
+        updateStatusTag.style.color = "red";
     };
-    //return to homepage once submitted successfully,
+    //render prospective snack page with updated properties once submitted successfully,
     let redirect_Page = () => {
         if (response.status === 200) {
             let tID = setTimeout(function () {
