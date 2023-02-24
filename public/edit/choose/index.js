fa83676205a2863ffc05904afab9464feffe14a8
Jose Castro <JoseAneurys@me.com>
2023-02-23 23:56:27 -0500

    Cleaning code -- Edit/Choose complete

--- public/edit/choose/index.js
+++ public/edit/choose/index.js
@@ -7,28 +7,32 @@ homepageClick.addEventListener('click', () => {
     window.location.href = "../../"
 });
 
-//make fetch request for all items
+//display current snacks in database available for editing
 const getAndDisplayData = async () => {
+    //make call to respective server route for retrieval of snacks in database
     let response = await fetch("http://localhost:5000/get_snacks");
+    //set information sent back from database to server at above route as value of the variable "data"
     let data = await response.json()
-    console.log(data);
+    //access the div in HTML file with respective ID by creating variable with a value of the ID in document
     let parentContainer = document.getElementById("container")
     //map through data and for each item add to screen
     data.forEach(item => {
+        //create a div to contain each item 
         let itemContainer = document.createElement("div");
+        //define the div with its contents
         itemContainer.innerHTML = `
         <h1>${item.snack}</h1>
         <a href="./snack" target="_blank">
         <img src=${item.image} width="200" length="200"/>
         </a>
         `
+        //add each newly contained and defined items to the parent div
+        parentContainer.appendChild(itemContainer);
 
         //go to specific snack editing page once image is clicked
         itemContainer.addEventListener("click", () => {
-            console.log("click", item._id);
             window.location.href=`../../edit/editSnack/?id=${item._id}`
         })
-        parentContainer.appendChild(itemContainer);
 
         //hide snacks from homepage if none available
         if (item.quantity < 1) {
@@ -39,5 +43,5 @@ const getAndDisplayData = async () => {
     });
 }
 
+//call the function
 getAndDisplayData();
-//display data on screen
